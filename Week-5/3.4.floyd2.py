4b5baec83ab98c2989a19d23ebb56b7cbeab8f02
King_Eon <0302tjddjs@gmail.com>
2025-10-08 15:59:06 +0900

    Week5

--- Week-5/3.4.floyd2.py
+++ Week-5/3.4.floyd2.py
@@ -4,7 +4,11 @@ def floyd2(n: int, W: List[List[int]]) -> List[List[int]]:
     P = [[-1] * (n) for _ in range(n)]
     D = W
     # Complete the code here
-    
+    for k in range(n):
+       for i in range(n):
+           for j in range(n):
+               D[i][j] = min(D[i][j], D[i][k] + D[k][j])
+               P[i][j] = k
     return D, P
 
 def path(i: int, j: int, P: list[list[int]]):
