b38efc5bc0d1b78df148337e0a9347ba345f0dd9
King_Eon <0302tjddjs@gmail.com>
2025-11-26 17:36:51 +0900

    week12

--- Week-12/7.1.insertion.sort.py
+++ Week-12/7.1.insertion.sort.py
@@ -2,3 +2,13 @@ from typing import List
 
 def insertionsort(n: int, S: List[int]) -> None:
     # Complete the code here
+    for i in range(1, n):
+        key = S[i]
+        j = i - 1
+        # Move elements of S[0..i-1], that are greater than key,
+        # to one position ahead of their current position
+        while j >= 0 and S[j] > key:
+            S[j + 1] = S[j]
+            j -= 1
+        S[j + 1] = key
+    return
--- Week-12/7.2.selection.sort.py
+++ Week-12/7.2.selection.sort.py
@@ -2,3 +2,10 @@ from typing import List
 
 def selectionsort(n: int, S: List[int]) -> None:
     # Complete the code here
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if S[j] < S[min_index]:
+                min_index = j
+        S[i], S[min_index] = S[min_index], S[i]
+    return
